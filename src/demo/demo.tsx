d3b88d86775896fb89ece2dd1d4606e888654840
DESKTOP-GS19N8E\thoma <thomasgonda3@gmail.com>
2020-11-19 12:07:28 -0800

    Added React app

---
+++ src/demo/demo.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import 'regenerator-runtime/runtime'
+
+class App extends Component<{}, {}> {
+  render(): JSX.Element {
+    return <div>Hello!</div>
+  }
+}
+export default App
