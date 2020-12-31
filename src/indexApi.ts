df9d05a8e8f21fccc2a208175873c935cceb7ecb
DESKTOP-GS19N8E\thoma <thomasgonda3@gmail.com>
2020-12-30 17:02:01 -0800

    increased size limit to 8mb

--- src/indexApi.ts
+++ src/indexApi.ts
@@ -108,7 +108,7 @@ function main(): void {
   const app = express()
   const logsRecords = nanoDb.use('logs_records')
 
-  app.use(express.json())
+  app.use(express.json({ limit: '8mb' }))
   app.use(cors())
   app.use('/', express.static('dist'))
 
