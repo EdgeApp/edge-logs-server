215f454fb2cdcac8c32c602c6827989a97258afd
DESKTOP-GS19N8E\thoma <thomasgonda3@gmail.com>
2021-01-22 14:19:50 -0800

    combined common code in fetchApi

--- src/util.ts
+++ src/util.ts
@@ -17,32 +17,22 @@ export interface FetchLogParams {
   _id: string
 }
 
-const searchLogsEndpoint = `https://${config.logsServerAddress}/v1/findLogs/?`
-
-export const searchLogs = async (params: SearchLogsParams): Promise<any> => {
+const fetchApi = (endpoint: string) => async (params: any): Promise<any> => {
+  const uri = `https://${config.logsServerAddress}/v1/${endpoint}/?`
   const query = Object.keys(params)
     .map(param => {
       if (params[param] !== '') return `${param}=${params[param]}`
       return ''
     })
     .join('&')
-  const response = await fetch(searchLogsEndpoint + query)
-  if (!response.ok) return { data: [], status: response.status }
+  const response = await fetch(uri + query)
+  if (!response.ok && endpoint === 'findLogs')
+    return { data: [], status: response.status }
+  if (!response.ok && endpoint === 'getLog')
+    return { data: {}, status: response.status }
   const data = await response.json()
   return { data, status: response.status }
 }
 
-const fetchLogEndpoint = `https://${config.logsServerAddress}/v1/getLog/?`
-
-export const fetchLog = async (params: FetchLogParams): Promise<any> => {
-  const query = Object.keys(params)
-    .map(param => {
-      if (params[param] !== '') return `${param}=${params[param]}`
-      return ''
-    })
-    .join('&')
-  const response = await fetch(fetchLogEndpoint + query)
-  if (!response.ok) return { log: {}, status: response.status }
-  const log = await response.json()
-  return { log, status: response.status }
-}
+export const searchLogs = fetchApi('findLogs')
+export const fetchLog = fetchApi('getLog')
