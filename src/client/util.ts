fc6a473334702cd858542ce5fd7378833bbce1ca
Paul V Puey <paul@edge.app>
2023-02-22 22:51:25 -0800

    Run lint fix

--- src/client/util.ts
+++ src/client/util.ts
@@ -16,23 +16,25 @@ export interface FetchLogParams {
   withData?: boolean
 }
 
-const fetchApi = (endpoint: string) => async (params: any): Promise<any> => {
-  const uri = `${location.origin}/v1/${endpoint}/?`
-  const query = Object.keys(params)
-    .map(param => {
-      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
-      if (params[param] !== '') return `${param}=${params[param]}`
-      return ''
-    })
-    .join('&')
-  const response = await fetch(uri + query)
-  if (!response.ok && endpoint === 'findLogs')
-    return { data: [], status: response.status }
-  if (!response.ok && endpoint === 'getLog')
-    return { data: {}, status: response.status }
-  const data = await response.json()
-  return { data, status: response.status }
-}
+const fetchApi =
+  (endpoint: string) =>
+  async (params: any): Promise<any> => {
+    const uri = `${location.origin}/v1/${endpoint}/?`
+    const query = Object.keys(params)
+      .map(param => {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        if (params[param] !== '') return `${param}=${params[param]}`
+        return ''
+      })
+      .join('&')
+    const response = await fetch(uri + query)
+    if (!response.ok && endpoint === 'findLogs')
+      return { data: [], status: response.status }
+    if (!response.ok && endpoint === 'getLog')
+      return { data: {}, status: response.status }
+    const data = await response.json()
+    return { data, status: response.status }
+  }
 
 export const searchLogs = fetchApi('findLogs')
 export const fetchLog = fetchApi('getLog')
